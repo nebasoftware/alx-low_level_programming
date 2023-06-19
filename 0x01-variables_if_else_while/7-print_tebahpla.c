738189437a4fe9e0c7eb72e439a4ec865bc8b81b
Nebiyou Yemam <nebiyounebex@gmail.com>
2023-06-19 06:01:10 -0700

    task 7 Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 if completed successfully
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
