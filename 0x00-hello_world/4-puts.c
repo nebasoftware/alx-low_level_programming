f4a4b8ca4a240aba3eda56367cfb0c810bfcfb3e
Nebiyou Yemam <nebiyounebex@gmail.com>
2023-06-19 00:55:01 -0700

    task 4 Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*
- * main - entry point
- */ 
+ * main: entry point
+ *Return: returns 0 if successfull
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
