a579e3cfb04575185d7d9de5c756d6126d7dfc3f
Nebiyou Yemam <nebiyounebex@gmail.com>
2023-06-19 05:16:16 -0700

    task 3 alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 if completed successfully
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 97; i <= 122;i++)
+		putchar(i);
+
+	for (j = 65; j <= 90; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
