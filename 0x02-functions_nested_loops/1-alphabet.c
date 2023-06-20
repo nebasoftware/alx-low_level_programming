709088b8e0228d2c44cc2ec8fdd68975340214b8
Nebiyou Yemam <nebiyounebex@gmail.com>
2023-06-20 02:54:08 -0700

    task 1 I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - entrtry point
+ *
+ * Return: 0 if completed successfully
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
