e561e190b37eda1fa0606b05e8a469e9db5e7ab3
Nebiyou Yemam <nebiyounebex@gmail.com>
2023-06-22 06:05:43 -0700

    multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main = entry point
+ *
+ * Return: 0 if completed successfully
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
+
