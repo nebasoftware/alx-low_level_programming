988d90b1da74208e823e293b015cdbb06443de6f
Nebiyou Yemam <nebiyounebex@gmail.com>
2023-06-23 06:36:17 -0700

    task 4 Numbers constitute the only universal language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints number from 0 to 14 10 times
+ *
+ * Return: 0 if completed successfully
+ */
+
+void more_numbers(void)
+{
+	int j, i;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 14; i++)
+		{
+			if (i <= 9)
+				_putchar(i);
+			else
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
