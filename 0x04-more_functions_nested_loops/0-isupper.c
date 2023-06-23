e441a27e61de4a16f3fd9318cc281ef4dc09b554
Nebiyou Yemam <nebiyounebex@gmail.com>
2023-06-23 05:32:00 -0700

    task 0 isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/** 
+ * _isupper - checks for uppercase letters
+ * @c: passed value
+ * Return: 1 if completed successfully
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
