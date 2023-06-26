2b56e6dc7a1ddc49bb1224687d72fbcb32ab554e
Nebiyou Yemam <nebiyounebex@gmail.com>
2023-06-26 03:26:39 -0700

    task 9 Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 if completed successfully
+ */
+
+int main(void)
+{
+	int k;
+
+	for (k = 1; k < 101; k++)
+	{
+		if (k % 3 == 0)
+			printf("Fizz ");
+		else if (k % 5 == 0)
+			printf("Buzz ");
+		else if (k % 3 == 0 && k % 5 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", k);
+	}
+	printf("\n");
+
+	return (0);
+}
