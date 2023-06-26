42b515a11378bcd679511af0e0e0e1f04f3eaca7
Nebiyou Yemam <nebiyounebex@gmail.com>
2023-06-26 05:32:04 -0700

    task 2 this report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: passed string
+ * Return: 0 if completed succesfully
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+	}
+	return (length);
+}
