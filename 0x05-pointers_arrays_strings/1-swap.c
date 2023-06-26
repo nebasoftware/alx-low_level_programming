783d9ab8ad157484d8be6221c695a39e747bd917
Nebiyou Yemam <nebiyounebex@gmail.com>
2023-06-26 05:17:10 -0700

    task 1 don't swa[ horeses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap two integers
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
