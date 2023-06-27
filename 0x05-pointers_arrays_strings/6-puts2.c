96e32b389f4bf98e9389bbb1a6781c0c0035ed66
Nebiyou Yemam <nebiyounebex@gmail.com>
2023-06-27 04:31:49 -0700

    task 6 half the lies they tell about me aren 't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts - prints every other character
+ * @str: string to be worked on
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int length, i;
+
+	for (length = 0; str[length] != '\0'; length++)
+	{
+	}
+	for (i = 0; i <= length; i = i + 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
