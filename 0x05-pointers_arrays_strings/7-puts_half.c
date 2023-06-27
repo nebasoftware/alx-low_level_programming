4c3038909856e24ee3d8bc16d857cf743f3d4bb5
Nebiyou Yemam <nebiyounebex@gmail.com>
2023-06-27 05:13:56 -0700

    task 7 winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,14 +15,14 @@ void puts_half(char *str)
 	}
 	if ((length + 1) % 2 == 0)
 	{
-		for (i = length / 2; i < length; i++)
+		for (i = (length + 1) / 2; i < length; i++)
 		{
 			_putchar(str[i]);
 		}
 	}
 	else
 	{
-		for (i = (length - 1) / 2; i < length; i++)
+		for (i = length / 2; i < length; i++)
 		{
 			_putchar(str[i]);
 		}
