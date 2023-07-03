42a5aab82d84dacb590de86dd9ad80b1fa8f45d2
Nebiyou Yemam <nebiyounebex@gmail.com>
2023-07-03 03:58:26 -0700

    task 0 memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fulls the first n bytes of memory with b
+ * @s: string to be worked on
+ * @b: character to be filled in
+ * @n: bytes to to be filled in
+ * Return: returns the momory location
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (0);
+}
