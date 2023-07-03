975f80ae5ccf7b586e65259cffe699f2aca3808b
Nebiyou Yemam <nebiyounebex@gmail.com>
2023-07-03 04:40:37 -0700

    task 3 strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locates the first occurence of the character
+ * @s: string to be searched in
+ * @c: characted to be searched in s
+ * Return: returns pointer to the location of the character
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i, j;
+	static char dest[98];
+
+	if (s == 0)
+		return (NULL);
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == c)
+			break;
+	}
+	for (j = i; s[j] != '\0'; j++)
+	{
+		dest[j - i] = s[j];
+	}
+	dest[j] = '\0';
+	return (dest);
+}
